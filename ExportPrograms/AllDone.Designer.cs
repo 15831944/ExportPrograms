8b7bcbdf52bfda389fb082d3cdd5c29b874a7e7e
KC Juntunen <juntunenkc@lan.amstore.com>
2017-02-15 07:39:29 -0500

    Added random thumbsup pics

--- ExportPrograms/AllDone.Designer.cs
+++ ExportPrograms/AllDone.Designer.cs
@@ -28,73 +28,100 @@ namespace ExportPrograms
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::ExportPrograms.Properties.Resources.Thumbs_Up;
-            this.pictureBox1.ImageLocation = "";
-            this.pictureBox1.InitialImage = global::ExportPrograms.Properties.Resources.Thumbs_Up;
-            this.pictureBox1.Location = new System.Drawing.Point(9, 13);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(250, 312);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(65, 329);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(133, 30);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Way To Go!";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(70, 397);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(123, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "You\'re welcome!";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(48, 359);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(166, 34);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Thanks for exporting\r\nprograms to the machines!";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // AllDone
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(268, 432);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
-            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.Name = "AllDone";
-            this.Text = "AllDone";
-            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(249)))), ((int)(((byte)(235)))));
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.button1 = new System.Windows.Forms.Button();
+      this.label2 = new System.Windows.Forms.Label();
+      this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.tableLayoutPanel1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.pictureBox1.ImageLocation = "";
+      this.pictureBox1.InitialImage = global::ExportPrograms.Properties.Resources.Thumbs_Up;
+      this.pictureBox1.Location = new System.Drawing.Point(3, 4);
+      this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(316, 312);
+      this.pictureBox1.TabIndex = 0;
+      this.pictureBox1.TabStop = false;
+      // 
+      // label1
+      // 
+      this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.Location = new System.Drawing.Point(3, 320);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(316, 30);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Way To Go!";
+      this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // button1
+      // 
+      this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.button1.Location = new System.Drawing.Point(3, 414);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(316, 23);
+      this.button1.TabIndex = 2;
+      this.button1.Text = "You\'re welcome!";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // label2
+      // 
+      this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(3, 350);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(316, 34);
+      this.label2.TabIndex = 3;
+      this.label2.Text = "Thanks for exporting\r\nprograms to the machines!";
+      this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // tableLayoutPanel1
+      // 
+      this.tableLayoutPanel1.ColumnCount = 1;
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+      this.tableLayoutPanel1.Controls.Add(this.button1, 0, 3);
+      this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+      this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
+      this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+      this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+      this.tableLayoutPanel1.RowCount = 4;
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+      this.tableLayoutPanel1.Size = new System.Drawing.Size(322, 440);
+      this.tableLayoutPanel1.TabIndex = 4;
+      // 
+      // AllDone
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(322, 440);
+      this.Controls.Add(this.tableLayoutPanel1);
+      this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      this.Name = "AllDone";
+      this.Text = "AllDone";
+      this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(249)))), ((int)(((byte)(235)))));
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.tableLayoutPanel1.ResumeLayout(false);
+      this.tableLayoutPanel1.PerformLayout();
+      this.ResumeLayout(false);
 
         }
 
@@ -104,5 +131,6 @@ namespace ExportPrograms
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
--- ExportPrograms/AllDone.cs
+++ ExportPrograms/AllDone.cs
@@ -10,11 +10,28 @@ namespace ExportPrograms {
   public partial class AllDone : Form {
     public AllDone() {
       InitializeComponent();
-      this.StartPosition = FormStartPosition.CenterParent;
+      List<Bitmap> bmps = new List<Bitmap>();
+      bmps.AddRange(new Bitmap[] {
+        Properties.Resources.meaning_of_vault_boy_thumbs_up_jpg,
+        Properties.Resources.sarcastic_thumbs_up,
+        Properties.Resources.Thumbs_Up,
+        Properties.Resources.Thumbs_Up_1_copy,
+        Properties.Resources.thumbs_up_bciy,
+        Properties.Resources.thumbs_up_through_wall_T,
+        Properties.Resources.thumbsAmmo03,
+        Properties.Resources.thumbsup6
+      });
+      Random r = new Random();
+      int selection = r.Next(0, bmps.Count);
+      pictureBox1.Size = bmps[selection].Size;
+      pictureBox1.Image = bmps[selection];
+      StartPosition = FormStartPosition.CenterParent;
+      Size s = new Size(bmps[selection].Size.Width + 5, bmps[selection].Size.Height + 160);
+      this.Size = s;
     }
 
     private void button1_Click(object sender, EventArgs e) {
-      this.Close();
+      Close();
     }
   }
 }
