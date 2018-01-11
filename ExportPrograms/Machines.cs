7a64e7b8ba42c2301bfd7973e5408a6e191b9ebc
KC Juntunen <juntunenkc@lan.amstore.com>
2018-01-11 08:53:16 -0500

    Tabs; throw without variables.

--- ExportPrograms/Machines.cs
+++ ExportPrograms/Machines.cs
@@ -4,43 +4,43 @@ using System.Data.SqlClient;
 namespace ExportPrograms {
 
 
-  public partial class Machines {
+	public partial class Machines {
 
-    public static void IncrementOdometer(int funcCode, int uid) {
-      SqlConnection conn = new SqlConnection(Properties.Settings.Default.ENGINEERINGConnectionString);
-      int rowAffected = 0;
-      string SQL = @"UPDATE GEN_ODOMETER SET ODO = ODO + 1 WHERE (FUNCID = @app AND USERID = @user);";
-      if (conn.State == ConnectionState.Closed) {
-        conn.Open();
-      }
-      using (SqlCommand comm = new SqlCommand(SQL, conn)) {
-        comm.Parameters.AddWithValue("@app", funcCode);
-        comm.Parameters.AddWithValue("@user", uid);
-        try {
-          rowAffected = (int)comm.ExecuteNonQuery();
-        } catch (InvalidOperationException ioe) {
-          throw ioe;
-        }
-      }
+		public static void IncrementOdometer(int funcCode, int uid) {
+			SqlConnection conn = new SqlConnection(Properties.Settings.Default.ENGINEERINGConnectionString);
+			int rowAffected = 0;
+			string SQL = @"UPDATE GEN_ODOMETER SET ODO = ODO + 1 WHERE (FUNCID = @app AND USERID = @user);";
+			if (conn.State == ConnectionState.Closed) {
+				conn.Open();
+			}
+			using (SqlCommand comm = new SqlCommand(SQL, conn)) {
+				comm.Parameters.AddWithValue("@app", funcCode);
+				comm.Parameters.AddWithValue("@user", uid);
+				try {
+					rowAffected = (int)comm.ExecuteNonQuery();
+				} catch (InvalidOperationException) {
+					throw;
+				}
+			}
 
-      if (rowAffected == 0) {
-        SQL = @"INSERT INTO GEN_ODOMETER (ODO, FUNCID, USERID) VALUES (@odo, @app, @user);";
+			if (rowAffected == 0) {
+				SQL = @"INSERT INTO GEN_ODOMETER (ODO, FUNCID, USERID) VALUES (@odo, @app, @user);";
 
-        using (SqlCommand comm = new SqlCommand(SQL, conn)) {
-          comm.Parameters.AddWithValue("@odo", 1);
-          comm.Parameters.AddWithValue("@app", (int)funcCode);
-          comm.Parameters.AddWithValue("@user", uid);
-          try {
-            rowAffected = (int)comm.ExecuteNonQuery();
-          } catch (InvalidOperationException ioe) {
-            throw ioe;
-          }
-        }
-      }
-      conn.Close();
-    }
-  }
+				using (SqlCommand comm = new SqlCommand(SQL, conn)) {
+					comm.Parameters.AddWithValue("@odo", 1);
+					comm.Parameters.AddWithValue("@app", (int)funcCode);
+					comm.Parameters.AddWithValue("@user", uid);
+					try {
+						rowAffected = (int)comm.ExecuteNonQuery();
+					} catch (InvalidOperationException) {
+						throw;
+					}
+				}
+			}
+			conn.Close();
+		}
+	}
 
-  partial class CUT_MACHINESDataTable {
-  }
+	partial class CUT_MACHINESDataTable {
+	}
 }
